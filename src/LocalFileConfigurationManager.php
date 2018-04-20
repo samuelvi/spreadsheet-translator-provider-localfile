c9e419b1d79b36e8930ac4e4d847ac50700e12f4
Samuel Vicent <samuelvicent@gmail.com>
2018-04-20 16:41:08 +0200

    First Commit

---
+++ src/LocalFileConfigurationManager.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Atico/SpreadsheetTranslator package.
+ *
+ * (c) Samuel Vicent <samuelvicent@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Atico\SpreadsheetTranslator\Provider\LocalFile;
+
+use Atico\SpreadsheetTranslator\Core\Configuration\ProviderConfigurationInterface;
+use Atico\SpreadsheetTranslator\Core\Provider\DefaultProviderManager;
+
+class LocalFileConfigurationManager extends DefaultProviderManager implements ProviderConfigurationInterface
+{
+}
---
+++ src/LocalFileProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Atico/SpreadsheetTranslator package.
+ *
+ * (c) Samuel Vicent <samuelvicent@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Atico\SpreadsheetTranslator\Provider\LocalFile;
+
+use Atico\SpreadsheetTranslator\Core\Configuration\Configuration;
+use Atico\SpreadsheetTranslator\Core\Provider\ProviderInterface;
+use Atico\SpreadsheetTranslator\Core\Resource\Resource;
+
+class LocalFileProvider implements ProviderInterface
+{
+    /** @var LocalFileConfigurationManager $configuration */
+    protected $configuration;
+
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = new LocalFileConfigurationManager($configuration);
+    }
+
+    public function getProvider()
+    {
+        return 'local_file';
+    }
+
+    public function handleSourceResource()
+    {
+        $tempLocalSourceFile = $this->configuration->getTempLocalSourceFile();
+        copy($this->configuration->getSourceResource(), $tempLocalSourceFile);
+        return new Resource($tempLocalSourceFile, $this->configuration->getFormat());
+    }
+}
